12ea2657225e65aa9ca77c38d6c5a9dcdfd5324b
Elaine <e.cristina00@gmail.com>
2022-12-11 18:41:20 -0300

    Métodos com parâmetros

--- src/javacore/conhecendometodos/domain/Calculadora.java
+++ src/javacore/conhecendometodos/domain/Calculadora.java
@@ -1,2 +1,16 @@
-package javacore.conhecendometodos.domain;public class Calculadora {
+package javacore.conhecendometodos.domain;
+
+public class Calculadora {
+
+    public double somar(){
+        return 10+30;
+    }
+
+    public double subtrair(){
+        return 30-10;
+    }
+
+    public int multiplicar (int num1, int num2){
+        return num1 *num2;
+    }
 }
--- src/javacore/conhecendometodos/test/CalculadoraTest.java
+++ src/javacore/conhecendometodos/test/CalculadoraTest.java
@@ -1,2 +1,14 @@
-package javacore.conhecendometodos.test;public class CalculadoraTest {
+package javacore.conhecendometodos.test;
+
+import javacore.conhecendometodos.domain.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println(calculadora.somar());
+        System.out.println(calculadora.subtrair());
+        System.out.println(calculadora.multiplicar(10, 2));
+    }
 }
